9bd84419af992fd4f29d75271f79b6ef6ab569cf
Tae90 <xogus54@gmail.com>
2024-11-13 13:38:59 +0900

    [feat] 공통 객체 타입 선언

---
+++ board-back/src/main/java/com/choongang/board_back/dto/object/CommentListItem.java
@@ -0,0 +1,8 @@
+package com.choongang.board_back.dto.object;
+
+public class CommentListItem {
+    private String nickname;
+    private String profileImage;
+    private String writeDateTime;
+    private String content;
+}
